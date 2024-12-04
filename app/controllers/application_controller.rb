6e8ea883eab051286f74db2cdeb913e84f5f500a
Bouali <elias.bouali90@gmail.com>
2024-12-04 15:27:08 +0100

    recherche

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_specialties
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role])
+  end
+
+  private
+
+  def set_specialties
+    @specialties = Chef.distinct.pluck(:specialties)
   end
 end
